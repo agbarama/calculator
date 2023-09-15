bfb383a61cefb570772ccb8e19af7120a97463c1
agbarama <agbaramafavour4@gmail.com>
2023-09-15 22:06:05 +0100

    Cleaned comments

--- main.js
+++ main.js
@@ -1,41 +1,11 @@
-// var calBody = document.querySelector(".js-calculator-body");
-// var pads = calBody.querySelector(".pads");
-// var buttons = pads.querySelectorAll("button");
-
-// buttons.addEventListener("click", function(){
-//   displayInput.value += "2";
-// });
-
 function appendToResult(value) {
   let displayInput = (document.getElementById("result").value += value);
 }
 
-// function signUsed() {
-//   let signUsed = false;
-
-//   const inputField = document.getElementById(".result");
-//   inputField.addEventListener("input", function (event) {
-//     const inputValue = event.target.value;
-
-//     if (inputValue.match("+", "-", "/", "*")) {
-//       signUsed = true;
-//     } else {
-//       signUsed = false;
-//     }
-//   });
-
-//   const calculatorForm = document.querySelector(".calculator-form");
-//   calculatorForm.addEventListener("submit", function (event) {
-//     if (signUsed) {
-//       alert("Error");
-//       event.preventDefault();
-//     }
-//   });
-// }
-
 function calculate() {
   try {
     let expression = document.getElementById("result").value;
+
     // let result = eval(expression);
     document.getElementById("result").value = result;
 
@@ -69,12 +39,3 @@ document.addEventListener("keydown", function (event) {
     calculate();
   }
 });
-
-// function blockFunction() {
-//   const displayInput = (document.getElementById("result").value += value);
-//   if (displayInput === "+" && "-") {
-//     console.log("error");
-//   }
-// }
-
-// blockFunction();
